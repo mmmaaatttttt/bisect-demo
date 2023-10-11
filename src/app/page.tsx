9bbb89a933af22062f7778a7bd5bbf005cd9a533
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2023-10-11 09:31:13 -0700

    add jsx to page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
+import VideoPlayer from "./video-player";
+import { YouTubeEvent } from "react-youtube";
 
 export default function Home() {
+  const [player, setPlayer] = useState<YouTubeEvent["target"] | null>(null);
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if (player && playing) {
+      player.playVideo();
+    }
+  }, [player, playing]);
+
   return (
     <main className={styles.main}>
-      hello world
+      {!playing && (
+        <button onClick={() => setPlaying(true)} className={styles.button}>
+          do not press me
+        </button>
+      )}
+      <VideoPlayer
+        hidden={!playing}
+        onReady={(evt) => setPlayer((evt as YouTubeEvent).target)}
+      />
     </main>
-  )
+  );
 }
